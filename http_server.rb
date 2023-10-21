823e2243bfc187093533d26faf14f792f0dd996c
mohammed <mohammed@mohammeds-Air.bbrouter>
2023-10-21 13:23:22 +0530

    add ruby and git workflow

---
+++ http_server.rb
@@ -0,0 +1,32 @@
+require 'socket'
+
+server = TCPServer.new('0.0.0.0', 80)
+
+loop do
+  client = server.accept
+  request = client.readpartial(2048)
+
+  method, path, version = request.lines[0].split
+
+  puts "#{method} #{path} #{version}"
+
+  if path == "/healthcheck"
+    response = "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Content-Length: 2\r\n" +
+               "\r\n" +
+               "OK"
+  else
+    response = "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Content-Length: 15\r\n" +
+               "\r\n" +
+               "Well, hello there!"
+  end
+
+  # Send the HTTP/1.1 response
+  client.write(response)
+
+  client.close
+end
+
